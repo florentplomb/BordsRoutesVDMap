e3b7ee6bfb0b7a6d94c2fd7ee6c327b30c213487
florentplomb <plombf@gmail.com>
2015-05-21 11:15:02 +0200

    comment populate fonction

--- app/controllers/zoneCtrl.js
+++ app/controllers/zoneCtrl.js
@@ -49,93 +49,93 @@ router.route('/info')
 
   });
 
-router.route('/populateFleures')
-  .post(function(req, res, next) {
-    var cpt = 0;
-    Zone.find()
-      .exec(function(err, zones) {
-        if (err) return next(err);
-        Flore.find()
-          .exec(function(err, flores) {
-            if (err) return next(err);;
-            zones.forEach(function(zone) {
-              flores.forEach(function(flore) {
-                if (parseInt(zone.properties.ID_MAPINFO) === parseInt(flore.talus_id)) {
-                  if (!_.contains(zone.properties.flores, flore.id)) {
-                    zone.properties.flores.push(flore.id);
-                    zone.save(function(err, zoneSaved) {
-                      cpt++
-                      if (err) return next(err)
-                    });
-                  }
-                }
-              })
-            })
-            return res.status(200).send("Fleur ajouté au talus ok" + cpt + "Enregistrements");
+// router.route('/populateFleures')
+//   .post(function(req, res, next) {
+//     var cpt = 0;
+//     Zone.find()
+//       .exec(function(err, zones) {
+//         if (err) return next(err);
+//         Flore.find()
+//           .exec(function(err, flores) {
+//             if (err) return next(err);;
+//             zones.forEach(function(zone) {
+//               flores.forEach(function(flore) {
+//                 if (parseInt(zone.properties.ID_MAPINFO) === parseInt(flore.talus_id)) {
+//                   if (!_.contains(zone.properties.flores, flore.id)) {
+//                     zone.properties.flores.push(flore.id);
+//                     zone.save(function(err, zoneSaved) {
+//                       cpt++
+//                       if (err) return next(err)
+//                     });
+//                   }
+//                 }
+//               })
+//             })
+//             return res.status(200).send("Fleur ajouté au talus ok" + cpt + "Enregistrements");
 
-          })
-      })
-  });
-
-
-router.route('/unPopulateFleures')
-  .post(function(req, res, next) {
-    var cpt = 0;
-    Zone.find()
-      .exec(function(err, zones) {
-        zones.forEach(function(zone) {
-          if (zone.properties.flores) {
-            //             function unique(arr) {
-            //     return _.uniq(arr, JSON.stringify).length === arr.length;
-            // }
-
-            var arr2 = _.uniq(zone.properties.flores, function(item) {
-              return JSON.stringify(item);
-            });
-
-            zone.properties.flores = arr2;
-            zone.save(function(err, zoneSaved) {
-              if (err) return next(err)
-            });
-
-          };
-        })
-        return res.status(200).send("UnpopulateOK");
+//           })
+//       })
+//   });
 
 
+// router.route('/unPopulateFleures')
+//   .post(function(req, res, next) {
+//     var cpt = 0;
+//     Zone.find()
+//       .exec(function(err, zones) {
+//         zones.forEach(function(zone) {
+//           if (zone.properties.flores) {
+//             //             function unique(arr) {
+//             //     return _.uniq(arr, JSON.stringify).length === arr.length;
+//             // }
 
-      })
-  });
+//             var arr2 = _.uniq(zone.properties.flores, function(item) {
+//               return JSON.stringify(item);
+//             });
+
+//             zone.properties.flores = arr2;
+//             zone.save(function(err, zoneSaved) {
+//               if (err) return next(err)
+//             });
+
+//           };
+//         })
+//         return res.status(200).send("UnpopulateOK");
 
 
-router.route('/populateCommunes')
-  .post(function(req, res, next) {
-    var cpt = 0;
-    Zone.find()
-      .exec(function(err, zones) {
-        if (err) return next(err);
-        zones.forEach(function(zone) {
-          Commune.find({
-              geometry: {
-                $geoIntersects: {
-                  $geometry: zone.geometry
-                }
-              }
-            })
-            .exec(function(err, communes) {
-              console.log(communes);
-              if (err) return next(err);
-              communes.forEach(function(commune) {
-                //  if (_.contains(zone.properties.communes, commune.id)) {
-                zone.properties.communes.push(commune.id);
-                zone.save(function(err, zoneSaved) {
-                  cpt++
-                  if (err) return next(err)
-                });
-                //  }
-              });
 
-            });
+//       })
+//   });
+
+
+// router.route('/populateCommunes')
+//   .post(function(req, res, next) {
+//     var cpt = 0;
+//     Zone.find()
+//       .exec(function(err, zones) {
+//         if (err) return next(err);
+//         zones.forEach(function(zone) {
+//           Commune.find({
+//               geometry: {
+//                 $geoIntersects: {
+//                   $geometry: zone.geometry
+//                 }
+//               }
+//             })
+//             .exec(function(err, communes) {
+//               console.log(communes);
+//               if (err) return next(err);
+//               communes.forEach(function(commune) {
+//                 //  if (_.contains(zone.properties.communes, commune.id)) {
+//                 zone.properties.communes.push(commune.id);
+//                 zone.save(function(err, zoneSaved) {
+//                   cpt++
+//                   if (err) return next(err)
+//                 });
+//                 //  }
+//               });
+
+//             });
 
 
         });
--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var app = angular.module('app', ['leaflet-directive', 'angucomplete-alt', ]);
-var apiUrl = "http://localhost:3000/api";
+//var apiUrl = "http://localhost:3000/api";
 //var apiUrl = "http://florentplomb.ch/api";
-// var apiUrl = "http://fleurs-vd.herokuapp.com/api";
+ var apiUrl = "http://fleurs-vd.herokuapp.com/api";
 
 var underscore = angular.module('underscore', []);
 underscore.factory('_', function() {
